0cd981e76badac762b6d4e221414ca400b08f311
vu-luong <vubinhcht@gmail.com>
2022-04-22 13:40:29 +1000

    fix comparator in sortEventHandlersByPriority

--- src/main/java/com/tvd12/ezyfox/core/util/EzyEventHandlerLists.java
+++ src/main/java/com/tvd12/ezyfox/core/util/EzyEventHandlerLists.java
@@ -1,16 +1,13 @@
 package com.tvd12.ezyfox.core.util;
 
-import static com.tvd12.ezyfox.core.util.EzyEventHandlerAnnotations.getHandlerPriority;
-
+import java.util.Comparator;
 import java.util.List;
 
 public final class EzyEventHandlerLists {
 
     private EzyEventHandlerLists() {}
-    
+
     public static void sortEventHandlersByPriority(List<Object> handlers) {
-        handlers.sort((a, b) ->
-            getHandlerPriority(a) - getHandlerPriority(b)
-        );
+        handlers.sort(Comparator.comparingInt(EzyEventHandlerAnnotations::getHandlerPriority));
     }
 }
